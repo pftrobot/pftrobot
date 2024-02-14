6b39835545c6af17391d42f45daba4cff94a8b57
hwhinowa <jejinoes1@gmail.com>
2024-02-14 22:22:19 +0900

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://pftrobot.github.io/pftrobot' : '',
   images: {
     loader: 'imgix',
     path: process.env.NODE_ENV === 'production' ? 'https://pftrobot.github.io/pftrobot' : '/',
