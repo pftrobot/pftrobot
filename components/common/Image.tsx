46b3ba1e66b73c75de97a03bcd8ec6c9b79d3d09
hwhinowa <jejinoes1@gmail.com>
2024-02-14 22:37:17 +0900

    fix: deploy

--- components/common/Image.tsx
+++ components/common/Image.tsx
@@ -1,7 +1,7 @@
 import NextImage, { ImageLoaderProps, ImageProps } from 'next/image'
 
 export function cdnLoader({ src, width }: ImageLoaderProps) {
-    return `src=${process.env.NODE_ENV === 'production' ? `https://pftrobot.github.io/pftrobot${src}` : src}`
+    return `src=${process.env.NODE_ENV === 'production' ? `https://pftrobot.github.io/pftrobot${src}` : 'ttt'}`
 }
 
 const Image = (props: ImageProps) => {
